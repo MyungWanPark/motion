286497b26684f6c58572e12b76c28ea359862362
parkmyungwan <mywanpark@gmail.com>
2023-01-27 14:54:36 +0900

    feat: Wrap with PageItemComponent for buttons

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+import { Component } from './components/component';
 import { TodoComponet } from './components/page/item/todo.js';
 import { NoteComponet } from './components/page/item/note.js';
 import { ImageComponent } from './components/page/item/image.js';
 import { VideoComponent } from './components/page/item/video.js';
-import { PageComponent } from './components/page/page.js';
+import { PageComponent, Composable } from './components/page/page.js';
 
 class App {
-  private readonly page: PageComponent;
+  private readonly page: Component & Composable;
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
@@ -14,19 +15,19 @@ class App {
       'Image Title',
       'https://picsum.photos/600/300'
     );
-    image.attachTo(appRoot, 'beforeend');
+    this.page.addChild(image);
 
     const video = new VideoComponent(
       'video title',
       'https://www.youtube.com/watch?v=5Le4ykggp0g'
     );
-    video.attachTo(appRoot, 'beforeend');
+    this.page.addChild(video);
 
     const note = new NoteComponet('note title', 'note body');
-    note.attachTo(appRoot, 'beforeend');
+    this.page.addChild(note);
 
     const todo = new TodoComponet('todo title', 'todo');
-    todo.attachTo(appRoot, 'beforeend');
+    this.page.addChild(todo);
   }
 }
 
--- src/components/page/page.ts
+++ src/components/page/page.ts
@@ -1,7 +1,44 @@
-import { BaseComponent } from '../component.js';
+import { Component, BaseComponent } from '../component.js';
 
-export class PageComponent extends BaseComponent<HTMLUListElement> {
+const htmlString = `
+  <li class="page_item">
+    <section class="page_item_body"></section>
+    <div class="page_item_btn">
+      <button class="close">&times;</button>
+    </div>
+  </li>
+`;
+
+export interface Composable {
+  addChild(child: Component): void;
+}
+
+class PageItemComponent
+  extends BaseComponent<HTMLElement>
+  implements Composable
+{
+  constructor() {
+    super(htmlString);
+  }
+  addChild(child: Component) {
+    const container = this.element.querySelector(
+      '.page_item_body'
+    )! as HTMLElement;
+    child.attachTo(container);
+  }
+}
+
+export class PageComponent
+  extends BaseComponent<HTMLUListElement>
+  implements Composable
+{
   constructor() {
     super('<ul class="page">This is Page Components!</ul>');
+  }
+
+  addChild(section: Component) {
+    const item = new PageItemComponent();
+    item.addChild(section);
+    item.attachTo(this.element, 'beforeend');
   }
 }
